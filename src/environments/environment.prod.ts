0ed093259a0c2e5fbd02129f9ede972ac56dbee6
Manojkumar Sachidanandam <159610153+Smk2471@users.noreply.github.com>
2026-06-20 15:54:16 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // Replace with your deployed backend URL after deploying the API
   // e.g. 'https://task-tracker-api.up.railway.app/api'
-  apiUrl: '/api'
+  apiUrl: 'https://minitrackerbackend-production.up.railway.app/'
 };
